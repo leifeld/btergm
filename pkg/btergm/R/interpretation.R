82df5dc1d33acc8d7bc2790d5c68950122d8e8ca
leifeld <leifeld@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-06-17 15:19:30 +0000

    Made the interpret function ready for undirected and bipartite graphs and networks with structural zeros. Added examples for these cases to the examples section of the help page.

--- pkg/btergm/R/interpretation.R
+++ pkg/btergm/R/interpretation.R
@@ -12,7 +12,7 @@ dointerpret <- function(env, coefficients, type, i, j, t) {
   if (!is.list(i)) {
     i <- rep(list(i), length(env$networks))
     num.actors <- numeric()
-    for (k in t) {
+    for (k in t) {  # note that t can be a vector of time steps
       num.actors[k] <- nrow(as.matrix(env$networks[[k]]))
     }
     if (length(table(num.actors)) > 1) {
@@ -44,14 +44,19 @@ dointerpret <- function(env, coefficients, type, i, j, t) {
     results <- numeric()
     for (i in t) {
       env$networks[[i]][node_i[[i]], node_j[[i]]] <- 0
-      stat0 <- summary(remove.offset.formula(env$form), response = NULL)
+      stat0 <- summary(env$form, response = NULL)
       env$networks[[i]][node_i[[i]], node_j[[i]]] <- 1
-      stat1 <- summary(remove.offset.formula(env$form), response = NULL)
+      stat1 <- summary(env$form, response = NULL)
       chgstat <- stat1 - stat0
       if (length(chgstat) != length(coefficients)) {
         stop(paste("Number of coefficients and statistics differ.",
             "Did you fit a curved model? Curved models with non-fixed",
             "parameters are currently not supported."))
+      }
+      for (k in 1:length(coefficients)) {
+        if (coefficients[k] == -Inf && chgstat[k] == 0) {
+          coefficients[k] <- 0  # no struct zero dyad; replace -Inf to avoid NaN
+        }
       }
       lp <- t(chgstat) %*% cbind(coefficients)
       result <- c(1 / (1 + exp(-lp)))
@@ -61,14 +66,30 @@ dointerpret <- function(env, coefficients, type, i, j, t) {
     results <- results[!is.na(results)]
     names(results) <- paste("t =", t)
   } else if (type == "dyad") {
+    if (-Inf %in% coefficients) {
+      coefficients[coefficients == -Inf] <- -9e8
+      warning(paste("There are -Inf coefficients (possibly due to", 
+          "offset terms). To yield interpretable results, -Inf is", 
+          "approximated by a large negative number (-9e8). Note that", 
+          "this may be imprecise."))
+    }
     results <- list()
     for (i in t) {
+      # print error if undirected
+      if ((class(env$networks[[i]]) == "network" && 
+          !is.directed(env$networks[[i]])) || 
+          (class(env$networks[[i]]) == "matrix" && 
+          is.mat.directed(env$networks[[i]]) == FALSE)) {
+        stop(paste0("Network at t=", i, " is undirected. Dyadic ", 
+            "probabilities do not make sense in undirected networks. Try ", 
+            "type = \"tie\" instead!"))
+      }
       eta_mat <- matrix(NA, 2, 2)
       for (xi in 0:1) {
         for (xj in 0:1) {
           env$networks[[i]][node_i[[i]], node_j[[i]]] <- xi
           env$networks[[i]][node_j[[i]], node_i[[i]]] <- xj
-          stat <- summary(remove.offset.formula(env$form), response = NULL)
+          stat <- summary(env$form, response = NULL)
           if (length(stat) != length(coefficients)) {
             stop(paste("Number of coefficients and statistics differ.",
                 "Did you fit a curved model? Curved models with non-fixed",
@@ -91,6 +112,13 @@ dointerpret <- function(env, coefficients, type, i, j, t) {
     results <- results[!sapply(results, is.null)]
     names(results) <- paste("t =", t)
   } else if (type == "node") {
+    if (-Inf %in% coefficients) {
+      coefficients[coefficients == -Inf] <- -9e8
+      warning(paste("There are -Inf coefficients (possibly due to", 
+          "offset terms). To yield interpretable results, -Inf is", 
+          "approximated by a large negative number (-9e8). Note that", 
+          "this may be imprecise."))
+    }
     results <- list()
     for (i in t) {
       m <- length(node_i[[i]])
@@ -123,7 +151,7 @@ dointerpret <- function(env, coefficients, type, i, j, t) {
         ik <- node_i[[i]]
         jk <- node_j[[i]]
         env$networks[[i]][ik, jk] <- vecs[l, ]
-        stat <- summary(remove.offset.formula(env$form), response = NULL)
+        stat <- summary(env$form, response = NULL)
         if (length(stat) != length(coefficients)) {
           stop(paste("Number of coefficients and statistics differ.",
               "Did you fit a curved model? Curved models with non-fixed",
@@ -156,7 +184,6 @@ interpret.ergm <- function(object, formula = getformula(object),
   
   env <- tergmprepare(formula = formula, offset = FALSE, blockdiag = FALSE, 
       verbose = FALSE)
-  # parent.env(env) <- environment()
   
   # extract response network and adjust formula
   if (!is.null(target)) {
@@ -226,12 +253,19 @@ edgeprob <- function(object, parallel = c("no", "multicore", "snow"),
   Y <- NULL
   dyads <- NULL
   for (i in 1:length(env$networks)) {
-    imat <- matrix(rep(1:nrow(as.matrix(env$networks[[i]])), 
-        ncol(as.matrix(env$networks[[i]]))), 
-        nrow = nrow(as.matrix(env$networks[[i]])))
-    jmat <- matrix(rep(1:ncol(as.matrix(env$networks[[i]])), 
-        nrow(as.matrix(env$networks[[i]]))), 
-        nrow = nrow(as.matrix(env$networks[[i]])), byrow = TRUE)
+    mat <- as.matrix(env$networks[[i]])
+    imat <- matrix(rep(1:nrow(mat), ncol(mat)), nrow = nrow(mat))
+    if ((class(env$networks[[i]]) == "network" && 
+        is.bipartite(env$networks[[i]])) || 
+        (class(env$networks[[i]]) == "matrix" && 
+        is.mat.onemode(env$networks[[i]]) == FALSE)) {
+      mn <- nrow(mat) + 1
+      mx <- nrow(mat) + ncol(mat)
+      jmat <- matrix(rep(mn:mx, nrow(mat)), nrow = nrow(mat), byrow = TRUE)
+    } else {
+      jmat <- matrix(rep(1:ncol(mat), nrow(mat)), nrow = nrow(mat), 
+          byrow = TRUE)
+    }
     form.middle <- as.character(env$form)[[1]]
     form.left <- as.character(env$form)[[2]]
     form.right <- as.character(env$form)[[3]]
@@ -241,7 +275,8 @@ edgeprob <- function(object, parallel = c("no", "multicore", "snow"),
     Y <- c(Y, mpli$response)
     dyads <- rbind(dyads, cbind(mpli$predictor, i))
   }
-  term.names <- colnames(dyads)[-(length(colnames(dyads)):(length(colnames(dyads)) - 2))]
+  term.names <- colnames(dyads)[-(length(colnames(dyads)):(length(colnames(
+      dyads)) - 2))]
   term.names <- c(term.names, "i", "j", "t")
   dyads <- data.frame(dyads)
   colnames(dyads) <- term.names
@@ -305,7 +340,8 @@ edgeprob <- function(object, parallel = c("no", "multicore", "snow"),
       message(paste("Using multicore parallelization with", ncpus, "cores."))
     }
     a <- proc.time()
-    prob <- mclapply(1:nrow(subs), interpret.parallel, subs, env, coefs, mc.cores = ncpus)
+    prob <- mclapply(1:nrow(subs), interpret.parallel, subs, env, coefs, 
+        mc.cores = ncpus)
     prob <- unlist(prob)
     b <- proc.time()
     if (verbose == TRUE) {
@@ -314,7 +350,8 @@ edgeprob <- function(object, parallel = c("no", "multicore", "snow"),
     }
   } else {
     if (verbose == TRUE) {
-      message(paste0("Parallelization is switched off. Computing probabilities sequentially."))
+      message(paste0("Parallelization is switched off. Computing", 
+          "probabilities sequentially."))
     }
     a <- proc.time()
     prob <- sapply(1:nrow(subs), interpret.parallel, subs, env, coefs)
